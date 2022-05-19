80ef8d38d711f294290a1a0fc8c0135d0dbc01ce
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-19 15:37:53 +0100

    Exemple Insert

--- app/src/main/java/com/example/exemple1sqlite/MainActivity.java
+++ app/src/main/java/com/example/exemple1sqlite/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.exemple1sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
         Student s1 = new Student(1,"SSSSS","HHHHH");
         Student s2 = new Student(1,"AAAAA","BBBBB");
         Student s3 = new Student(1,"CCCCC","FFFFFF");
-        MyDB.insert_etudiant(db.getWritableDatabase(),s1);
+        if(MyDB.insert_etudiant(db.getWritableDatabase(),s1)==-1)
+            Toast.makeText(this, "INSERT Echoue", Toast.LENGTH_SHORT).show();
+
         MyDB.insert_etudiant(db.getWritableDatabase(),s2);
         MyDB.insert_etudiant(db.getWritableDatabase(),s3);
 
